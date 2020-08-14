be08369332af9af7636de7f86daed43e81b20cb5
HemaO <hema.omprakash@gmail.com>
2020-08-13 20:55:15 -0400

    completed coding new regs

--- contactcard.py
+++ contactcard.py
@@ -16,33 +16,37 @@ class User(ContactBook):
         Users.create(first_name=self.first_name, last_name=self.last_name, username=self.username)
 
 class Contact(ContactBook):
-    def __init__(self, first_name, last_name, phone, addr_line1, addr_line2, zipcode, username):
+    def __init__(self, first_name, last_name, email, phone, addr_line1, addr_line2, zipcode, username):
         super().__init__(first_name,last_name,username,"contact") 
+        self.email = email
         self.phone = phone
         self.addr_line1 = addr_line1
         self.addr_line2 = addr_line2
         self.zipcode = zipcode
 
     def add_contact(self):
-        Contacts.create(first_name = self.first_name, last_name = self.last_name, phone = self.phone, addr_line1 = self.addr_line1, addr_line2 = self.addr_line2, zipcode = self.zipcode, username = self.username)
+        Contacts.create(first_name = self.first_name, last_name = self.last_name, email =  self.email, phone = self.phone, addr_line1 = self.addr_line1, addr_line2 = self.addr_line2, zipcode = self.zipcode, username = self.username)
+
+    def findOne(self, username):
+        Contacts.get(username = username)
 
 
 print("Welcome to Contactbook")
-User_Input = input(f'''Choose from one of the following options(type in the numbers)
+user_input = input(f'''Choose from one of the following options(type in the numbers)
 1. Register
 2. Add Contact
 3. Find Contact 
 4. Display All
 ''')
-
-if (user_Input == 1):
-    first_name = input("Enter your first name")
-    last_name = input("Enter your last name")
-    username = input("Enter your username")
+print(user_input)
+if (int(user_input) == 1):
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    username = input("Enter your username: ")
     newuser =  User(first_name, last_name, username)
     newuser.add_user()
     print(f"You have been registered successfully!")
-else if (user_Input == 2):
+elif (int(user_input) == 2):
     username = input("Enter your username")
     first_name = input("Enter contact's first name")
     last_name = input("Enter contact's last name")
@@ -51,8 +55,18 @@ else if (user_Input == 2):
     addr_line1 = input("Enter contact's address(line 1)")
     addr_line2 = input("Enter contact's address(line 2)")
     zipcode = input("Enter contact's zip code")
-else if (user_input == 3):
+    newcontact =  Contact(first_name, last_name, email, phone, addr_line1, addr_line2, zipcode, username)
+    newcontact.add_contact()
+    print(f"You have been registered successfully!")
+elif (int(user_input) == 3):
     username = input("Enter your username")
+    first_name = input("Enter contact's first name")
+    last_name = input("Enter contact's last name")
+elif(int(user_input) == 4):
+    print("display all data")
+else:
+    print("No matching input")
+    
     
     
 
